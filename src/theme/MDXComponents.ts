1e69d31fc8ad582194c045d45c78b1271a24a5a6
conarin <53005059+conarin@users.noreply.github.com>
2024-02-10 01:33:50 +0900

    feat: Mention コンポーネントを MDXComponents に追加

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import Mention from '@site/src/components/Mention';
+
+export default {
+    ...MDXComponents,
+    Mention,
+};
