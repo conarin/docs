c98f97148e01eeaafab274123065b5c195cc572c
conarin <53005059+conarin@users.noreply.github.com>
2024-03-08 01:25:38 +0900

    style: 表記ゆれを修正

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Mention from '@site/src/components/Mention';
-import Button from "@site/src/components/Button";
+import Button from '@site/src/components/Button';
 
 export default {
     ...MDXComponents,
