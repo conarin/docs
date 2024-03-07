48d8b4a58b1b7a7d626fb7e8a23a7d9fa3ce90c2
conarin <53005059+conarin@users.noreply.github.com>
2024-03-08 00:39:30 +0900

    feat: ボタンのコンポーネントを追加

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import styles from './styles.module.css';
+
+interface ButtonProps {
+    children: React.ReactElement;
+    disabled?: boolean;
+    style?: 'primary' | 'secondary' | 'success' | 'danger';
+    to?: string;
+}
+
+export default function Button(props: ButtonProps) {
+    const {
+        children,
+        to,
+        disabled = false,
+        style = 'primary'
+    } = props;
+    
+    if (disabled) {
+        return (
+            <span className={clsx(styles.button, styles[style], styles.disabled)}>
+                {children}
+            </span>
+        );
+    } else {
+        return (
+            <Link to={to} className={clsx(styles.button, styles[style])}>
+                {children}
+            </Link>
+        );
+    }
+}
--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,8 +1,10 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Mention from '@site/src/components/Mention';
+import Button from "@site/src/components/Button";
 
 export default {
     ...MDXComponents,
     Mention,
+    Button
 };
