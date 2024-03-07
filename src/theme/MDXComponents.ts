5866323dbe50f60aca36a0688f5dcbc6ba7d4221
conarin <53005059+conarin@users.noreply.github.com>
2024-03-08 01:25:59 +0900

    chore: 不要なimportを削除

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Mention from '@site/src/components/Mention';
 import Button from '@site/src/components/Button';
