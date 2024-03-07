141d0f39f881e34f52df4c413bd218a02b20d43e
conarin <53005059+conarin@users.noreply.github.com>
2024-03-07 14:51:38 +0900

    style: 不要な改行を削除

--- src/components/Mention.tsx
+++ src/components/Mention.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 export default function Mention({children}) {
     return (
-        <span
-            className='mention'
-        >
+        <span className='mention'>
             {children}
         </span>
     );
