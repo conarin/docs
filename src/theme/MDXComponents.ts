e881f1b0f4e4413c98557357faa44da188b8f6e6
conarin <53005059+conarin@users.noreply.github.com>
2024-03-11 03:59:05 +0900

    chore: discord-components-reactを追加

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,9 +1,17 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import Mention from '@site/src/components/Mention';
 import Button from '@site/src/components/Button';
+import {
+    DiscordMessages,
+    DiscordMessage,
+    DiscordMention
+} from '@skyra/discord-components-react';
 
 export default {
     ...MDXComponents,
     Mention,
-    Button
+    Button,
+    DiscordMessages,
+    DiscordMessage,
+    DiscordMention
 };
